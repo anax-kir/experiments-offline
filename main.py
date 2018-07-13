4ab3fa4e5409b2501c0e793cc39d8cfababbe4b5
anax-kir <anax.kir@yandex.com>
2018-07-14 01:59:32 +0300

    Fixed issue with encoding (by arimaglmm)

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from kivy.app import App
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
+from kivy.lang.builder import Builder
 
 import os
 import sys
+import _locale
 
+_locale._getdefaultlocale = (lambda *args: ['en_US', 'utf8'])
 PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(PATH + '/screen_classes')
 
@@ -22,6 +25,7 @@ class DataScreen(Screen):
     Data Screen: show file contents so user can check that file was parsed correctly
     """
     text = StringProperty('')
+
 
 class StartScreen(Screen):
     """
@@ -42,8 +46,12 @@ class ExperimentApp(App):
     Main Application Class
     """
     def build(self):
+        with open("screen_classes/socioling.kv", encoding="utf8") as socioling_file:
+            stream = socioling_file.read()
+        representation = Builder.load_string(stream, filename="screen_classes/socioling.kv")
         self.title = "Experiments: Layer 0"
         Window.clearcolor = (1, 1, 1, 1)
+        return representation
 
 
 if __name__ == '__main__':
